32c4e8f4789d969aab595ddb13bce2e814c36578
Talha Öztürk <talha.ozturk@cmosrealitylab.com>
2022-05-30 10:06:19 +0300

    sdk error

--- lib/views/mystory.dart
+++ lib/views/mystory.dart
@@ -2,7 +2,6 @@ import 'package:dusyeriinstagram/views/followers.dart';
 import 'package:dusyeriinstagram/views/posts.dart';
 import 'package:dusyeriinstagram/views/setprofile.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_verification_code/flutter_verification_code.dart';
 import 'login.dart';
 
 class MyStory extends StatefulWidget {
@@ -276,8 +275,7 @@ class _MyStoryState extends State<MyStory> with TickerProviderStateMixin {
               ),
               itemCount: myimages.length,
               itemBuilder: (BuildContext context, int index) {
-                bool _onEditing = true;
-                String _code;
+               
 
                 return InkWell(
                   onTap: () {
@@ -382,58 +380,7 @@ class _MyStoryState extends State<MyStory> with TickerProviderStateMixin {
                                                           CrossAxisAlignment
                                                               .center,
                                                       children: [
-                                                        VerificationCode(autofocus: true,
-                                                          textStyle: TextStyle(
-                                                              fontSize: 20.0,
-                                                              color: Colors
-                                                                  .red[900]),
-                                                          keyboardType:
-                                                              TextInputType
-                                                                  .number,
-                                                          underlineColor: Colors
-                                                              .grey, // If this is null it will use primaryColor: Colors.red from Theme
-                                                          length: 4,
-                                                          cursorColor: Colors
-                                                              .blue, // If this is null it will default to the ambient
-                                                          // clearAll is NOT required, you can delete it
-                                                          // takes any widget, so you can implement your design
-                                                          clearAll: Padding(
-                                                            padding:
-                                                                const EdgeInsets
-                                                                    .all(8.0),
-                                                            child: Text(
-                                                              'Temizle',
-                                                              style: TextStyle(
-                                                                  fontSize:
-                                                                      14.0,
-                                                                  decoration:
-                                                                      TextDecoration
-                                                                          .underline,
-                                                                  color: Colors
-                                                                          .blue[
-                                                                      700]),
-                                                            ),
-                                                          ),
-                                                          onCompleted:
-                                                              (String value) {
-                                                            setState(() {
-                                                              _code = value;
-                                                              
-                                                              
-                                                            });
-                                                          },
-                                                          onEditing:
-                                                              (bool value) {
-                                                            setState(() {
-                                                              _onEditing =
-                                                                  value;
-                                                            });
-                                                            if (!_onEditing)
-                                                              FocusScope.of(
-                                                                      context)
-                                                                  .unfocus();
-                                                          },
-                                                        ),
+                                                       
                                                       ],
                                                     ),
                                                   ],
